7f16381ea04dfd46666a7e1d8fa8c52f53b25e73
Pareen Patel <pareendpatel@gmail.com>
2020-07-05 19:08:08 -0700

    Added section one (html and css).

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const menu = document.querySelector('.nav__right-menu');
+const toggle = document.querySelector('.nav__center');
 menu.addEventListener('click', () => {
     menu.classList.toggle('active');
+    toggle.classList.toggle('menu-toggle');
 })
